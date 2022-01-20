92792c834e675dbac4d31e113575a988a5489734
jcpcabanada <jcpcabanada@gmail.com>
2022-01-20 04:46:34 -0800

    5th

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -8,21 +8,19 @@ const db = require('../../data/db-config')
   }
 */
 const checkSchemeId = async (req, res, next) => {
-try {
-  const exists = await db('schemes')
-      .where('scheme_id', req.params.scheme_id)
-      .first()
-
-
-  if(!exists){
-    next({
-      status: 404,
-      message: `scheme with scheme_id ${req.params.scheme_id} not found`
-    })
-  }
-} catch (err) {
-  next(err)
-}
+    try {
+        const existing = await db('schemes')
+            .where('scheme_id', req.params.scheme_id)
+            .first()
+        if (!existing) {
+            const error = {status: 404, message: `scheme with scheme_id ${req.params.scheme_id} not found`}
+            next(error)
+        } else {
+            next()
+        }
+    } catch (err) {
+        next(err)
+    }
 }
 
 /*
@@ -34,19 +32,19 @@ try {
   }
 */
 const validateScheme = (req, res, next) => {
-  const { scheme_name } = req.body
-if (
-    scheme_name === undefined ||
-    typeof scheme_name !== 'string' ||
-    !scheme_name.trim()
-) {
- next({
-   status: 400,
-   message: 'invalid scheme_name'
- })
-} else {
-  next()
-}
+    const {scheme_name} = req.body
+    if (
+        scheme_name === undefined ||
+        typeof scheme_name !== 'string' ||
+        !scheme_name.trim()
+    ) {
+        next({
+            status: 400,
+            message: 'invalid scheme_name'
+        })
+    } else {
+        next()
+    }
 
 }
 
@@ -60,24 +58,24 @@ if (
   }
 */
 const validateStep = (req, res, next) => {
-  const {instructions, step_number} = req.body
+    const {instructions, step_number} = req.body
 
-  if (
-      instructions === undefined ||
-      typeof instructions !== 'string' ||
-      !instructions.trim() ||
-      typeof step_number !== 'number' ||
-      step_number < 1
-  ) {
-    const error = {status: 400, message: 'invalid step'}
-    next(error)
-  } else {
-    next()
-  }
+    if (
+        instructions === undefined ||
+        typeof instructions !== 'string' ||
+        !instructions.trim() ||
+        typeof step_number !== 'number' ||
+        step_number < 1
+    ) {
+        const error = {status: 400, message: 'invalid step'}
+        next(error)
+    } else {
+        next()
+    }
 }
 
 module.exports = {
-  checkSchemeId,
-  validateScheme,
-  validateStep,
+    checkSchemeId,
+    validateScheme,
+    validateStep,
 }
